aed53a9982147f66cabcc86c2085143013606804
fatimatunnura <fatima.rini786@gmail.com>
2014-12-23 21:41:47 +0600

    Create list.php

---
+++ list.php
@@ -0,0 +1,32 @@
+<?php
+$link = mysqli_connect("localhost", "root", "rini07", "students");
+$query = "SELECT * FROM users;";
+$result = mysqli_query($link, $query);
+?>
+<ul>
+    <li><a href="#">Download as XL</a> </li>
+    <li><a href="#">Download as PDF</a> </li>
+    <li><a href="create.html">Create New Record</a> </li>
+</ul>
+<table border="1" width="70%">
+    <tr>
+        <td>ID</td>
+        <td>First Name</td>
+        <td>Last Name</td>
+        <td>Phone_No.</td>
+        <td>Action</td>
+    </tr>
+    <?php
+    foreach($result as $row){
+        ?>
+        <tr>
+            <td><?php echo $row['id']?></td>
+            <td><?php echo $row['first_name']?></td>
+            <td><?php echo $row['last_name']?></td>
+            <td><?php echo $row['phone']?></td>
+            <td> <a href="#">Edit</a> | <a href="delete.php?id=<?php echo $row['id']?>">Delete</a> | <a href="view.php?id=<?php echo $row['id']?>">View</a></td>
+        </tr>
+    <?php
+    }
+    ?>
+</table>
