1071fa10fc84eaf9638b45ce63ec62888ca718a6
fatimatunnura <fatima.rini786@gmail.com>
2014-12-23 21:40:47 +0600

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,7 @@
+<?php
+$id = $_GET['id'];
+$link = mysqli_connect("localhost", "root", "rini07", "students");
+$query = "DELETE FROM `students`.`users` WHERE `users`.`id` = $id";
+mysqli_query($link, $query);
+header('location:list.php');
+?>
